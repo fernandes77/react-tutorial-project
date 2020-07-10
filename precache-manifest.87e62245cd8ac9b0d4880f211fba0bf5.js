af2a1987b35907fd3922b6893d77b9b8f8c76b74
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2020-07-10 17:18:05 -0300

    Updates

---
+++ precache-manifest.87e62245cd8ac9b0d4880f211fba0bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efd46d77e229c6071c557edafdbe333",
+    "url": "/react-tutorial-project/index.html"
+  },
+  {
+    "revision": "f5a91a45d41d6f1caca7",
+    "url": "/react-tutorial-project/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "ae7c13f09a8f3ed40983",
+    "url": "/react-tutorial-project/static/js/2.6d88c8c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial-project/static/js/2.6d88c8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a91a45d41d6f1caca7",
+    "url": "/react-tutorial-project/static/js/main.f0ceddb7.chunk.js"
+  },
+  {
+    "revision": "905e6531efb4d1e3d7a4",
+    "url": "/react-tutorial-project/static/js/runtime-main.f23f61b8.js"
+  }
+]);
